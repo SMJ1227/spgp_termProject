36e6a118d4d2e280e0caed7dccd213d293ff0225
SMJ1227 <112992077+SMJ1227@users.noreply.github.com>
2024-05-05 01:56:06 +0900

    laser를 sword_effect로 변경

--- app/src/main/java/com/example/game/game/Bullet.java
+++ app/src/main/java/com/example/game/game/Bullet.java
@@ -10,20 +10,20 @@ import com.example.game.framework.scene.RecycleBin;
 import com.example.game.framework.scene.Scene;
 
 public class Bullet extends Sprite implements IBoxCollidable, IRecyclable {
-    private static final float BULLET_WIDTH = 0.4f;
-    private static final float BULLET_HEIGHT = BULLET_WIDTH / 3;
+    private static final float BULLET_WIDTH = 1f;
+    private static final float BULLET_HEIGHT = BULLET_WIDTH;
     private static final float SPEED = 20.0f;
     private int power;
     private Bullet(float x, float y, int power) {
-        super(R.mipmap.laser_1);
-        setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
+        super(R.mipmap.sword_effect);
+        setPosition(x, y+0.4f, BULLET_WIDTH, BULLET_HEIGHT);
         this.power = power;
         dx = SPEED;
     }
     public static Bullet get(float x, float y, int power) {
         Bullet bullet = (Bullet) RecycleBin.get(Bullet.class);
         if (bullet != null) {
-            bullet.setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
+            bullet.setPosition(x, y + 0.4f, BULLET_WIDTH, BULLET_HEIGHT);
             bullet.power = power;
             return bullet;
         }
