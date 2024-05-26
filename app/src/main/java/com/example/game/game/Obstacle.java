1fb04962420bd78cf2237f410cfe8ddc931c93a1
SMJ1227 <112992077+SMJ1227@users.noreply.github.com>
2024-05-26 23:59:59 +0900

    disturb이미지 재사용

--- app/src/main/java/com/example/game/game/Obstacle.java
+++ app/src/main/java/com/example/game/game/Obstacle.java
@@ -10,14 +10,28 @@ public class Obstacle extends MapObject {
 
     protected void init(int index, float left, float top) {
         int x = random.nextInt(3);
-        if(x == 0){
-            init(left, top - 1.0f, R.mipmap.epn01_tm01_jp1a);
-        }
-        else if(x == 1){
-            init(left, top - 3.0f, R.mipmap.epn01_tm01_jp1a);
+        int mimmap = random.nextInt(2);
+        if(mimmap == 0){
+            if(x == 0){
+                init(left, top - 1.0f, R.mipmap.epn01_tm01_jp1a);
+            }
+            else if(x == 1){
+                init(left, top - 3.0f, R.mipmap.epn01_tm01_jp1a);
+            }
+            else{
+                init(left, top - 5.0f, R.mipmap.epn01_tm01_jp1a);
+            }
         }
         else{
-            init(left, top - 5.0f, R.mipmap.epn01_tm01_jp1a);
+            if(x == 0){
+                init(left, top - 1.0f, R.mipmap.disturb);
+            }
+            else if(x == 1){
+                init(left, top - 3.0f, R.mipmap.disturb);
+            }
+            else{
+                init(left, top - 5.0f, R.mipmap.disturb);
+            }
         }
     }
     protected void init(float left, float top, int resId) {
