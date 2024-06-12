7404f5b4b6fe69b6c07cf2cbb63f9bea7aad20dc
SMJ1227 <112992077+SMJ1227@users.noreply.github.com>
2024-06-12 19:28:28 +0900

    버튼 위치 변경

--- app/src/main/java/com/example/game/game/MainScene.java
+++ app/src/main/java/com/example/game/game/MainScene.java
@@ -64,35 +64,35 @@ public class MainScene extends Scene {
                 }
             }));
         }
-        add(Layer.touch, new Button(R.mipmap.btn_shoot_n, 12.5f, 8.5f, 2.0f, 0.75f, new Button.Callback() {
+        add(Layer.touch, new Button(R.mipmap.btn_shoot_n, 14.5f, 7.7f, 2.0f, 0.75f, new Button.Callback() {
             @Override
             public boolean onTouch(Button.Action action) {
                 player.throwing();
                 return true;
             }
         }));
-        add(Layer.touch, new Button(R.mipmap.btn_jump_n, 14.5f, 7.7f, 2.0f, 0.75f, new Button.Callback() {
+        add(Layer.touch, new Button(R.mipmap.btn_jump_n, 2.0f, 7.5f, 2.0f, 0.75f, new Button.Callback() {
             @Override
             public boolean onTouch(Button.Action action) {
                 player.jump();
                 return false;
             }
         }));
-        add(Layer.touch, new Button(R.mipmap.btn_fall_n, 14.5f, 8.5f, 2.0f, 0.75f, new Button.Callback() {
+        add(Layer.touch, new Button(R.mipmap.btn_fall_n, 13.5f, 8.5f, 2.0f, 0.75f, new Button.Callback() {
             @Override
             public boolean onTouch(Button.Action action) {
                 player.fall();
                 return false;
             }
         }));
-        add(Layer.touch, new Button(R.mipmap.btn_right_n, 3.0f, 8.0f, 1.0f, 1.0f, new Button.Callback() {
+        add(Layer.touch, new Button(R.mipmap.btn_right_n, 3.0f, 8.3f, 1.0f, 1.0f, new Button.Callback() {
             @Override   // 오
             public boolean onTouch(Button.Action action) {
                 player.running(action);
                 return false;
             }
         }));
-        add(Layer.touch, new Button(R.mipmap.btn_left_n, 1.5f, 8.0f, 1.0f, 1.0f, new Button.Callback() {
+        add(Layer.touch, new Button(R.mipmap.btn_left_n, 1.0f, 8.3f, 1.0f, 1.0f, new Button.Callback() {
             @Override   // 왼
             public boolean onTouch(Button.Action action) {
                 player.goBack(action);
@@ -109,7 +109,7 @@ public class MainScene extends Scene {
         this.score = new Score(R.mipmap.number_24x32, 8.5f, 0.5f, 0.6f);
         score.setScore(0);
         add(Layer.ui, score);
-        add(Layer.item, new Item(R.mipmap.throw_sword, 13.8f, 7.0f, 0.6f, 0));
+        add(Layer.item, new Item(R.mipmap.throw_sword, 15.3f, 7.0f, 0.6f, 0));
         add(Layer.item, new Item(R.mipmap.heart, 0.1f, 0.3f, 1.0f, 1));
     }
 
@@ -186,7 +186,7 @@ public class MainScene extends Scene {
 
         // 화염 게이지 그리기
         canvas.save();
-        canvas.translate(12.0f, 8.7f); // 화염 게이지 위치 설정 (btn_attack_n 버튼 아래)
+        canvas.translate(14.0f, 7.9f); // 화염 게이지 위치 설정 (btn_attack_n 버튼 아래)
         float fireCoolTime = Player.fireCoolTime;
         float fireInterval = player.getFireInterval();
         if (fireCoolTime > 0) {
